50210d6da42d8d0385b7dd8408a5a27666e4140f
sanielsand27 <114727600+sanielsand27@users.noreply.github.com>
2026-09-22 17:30:24 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,9 +2,24 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+pool
+  .promise()
+  .query("SELECT 1")
+  .then(() => {
+    console.log("DATABASE CONNECTED");
+  })
+  .catch((err) => {
+    console.error("DATABASE ERROR");
+    console.error(err);
+  });
 
 module.exports = pool.promise();
